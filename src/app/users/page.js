7b2ca02832453131997d14783e6955d793f3ffd9
seangleng <seangleng507@gmail.com>
2023-05-22 11:22:44 +0700

    done

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -4,7 +4,7 @@ import { BASE_URL } from '../constant/constant';
 
 export default function Users() {
     const [users, setUser] = useState([])
-    fetch(`${BASE_URL}users?limit=8`)
+    fetch(`${BASE_URL}users?limit=10`)
         .then(users => users.json())
         .then(response => setUser(response));
         console.log(users)
